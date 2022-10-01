a2ac73cb876999721a43c8b623ed9cd0a8efd670
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2022-10-01 02:32:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,59 @@
 # implement a libaries
+from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
+global draw_width
+global draw_height
 
-# TODO: write number in arabic
-# TODO: slim grid and graph from y side
-
-
-
-draw_width = 2                          #TODO:control view
 draw_height = 6
+draw_width = 6
 
-draw_height += .5
-draw_width += .4
+def draw_graph() :
+    draw_height += .5
+    draw_width += .5
 
-
-def draw_arrows():
     # draw vertical arrow with red color
-    plt.arrow(0, 0, 0, draw_height, color='red', head_width=0.1, head_length=0.2)
-    plt.arrow(0, 0, 0, -1 * draw_height, color='red', head_width=0.1, head_length=0.2)
+    plt.arrow(0, 0, 0, draw_height, color='red', head_width=0.2, head_length=0.2)
+    plt.arrow(0, 0, 0, -1 * draw_height, color='red', head_width=0.2, head_length=0.2)
 
     # draw horizontal arrow with red color
-    plt.arrow(0, 0, draw_width, 0, color='red', head_width=0.2, head_length=0.1)
-    plt.arrow(0, 0, -1 * draw_width, 0, color='red', head_width=0.2, head_length=0.1)
+    plt.arrow(0, 0, draw_width, 0, color='red', head_width=0.2, head_length=0.2)
+    plt.arrow(0, 0, -1 * draw_width, 0, color='red', head_width=0.2, head_length=0.2)
 
-draw_arrows()
-# draw y = x^3+x with red color
-x = np.arange(-1.8, 1.8, 0.01)
+    # draw y = x^3+x
+    x = np.arange(-draw_width, draw_width, 0.01)
+    y = x ** 3 + x
+    plt.plot(x, y)
 
-y = x  # put equation here
-plt.plot(x, y, color='red')
+    # plotting the points
+    plt.plot(x, y)
 
-# plotting the points
+    draw_height += 2
+    draw_width += 2
 
-draw_height +=.5
-draw_width += .2
+    # change mode two squares
+    plt.axis([-1 * draw_width, draw_width, -1 * draw_height, draw_height])
 
-# change mode two squares
-plt.axis([-1 * draw_width, draw_width, -1 * draw_height, draw_height])
-plt.grid(color='red', linewidth=0.5)
+def draw_grid () :
+    plt.grid(color='red', linewidth=0.5)
+    resolution_value = 512
+    plt.savefig("myImage.png", format="png", dpi=resolution_value)
 
-resolution_value = 512
-plt.savefig("myImage.png", format="png", dpi=resolution_value)
+    plt.show()
 
+def clip_photo ():
+    # mapping photo
+
+
+    # loading the image
+    img = PIL.Image.open("geeksforgeeks.png")
+
+    # fetching the dimensions
+    wid, hgt = img.size
+
+    # displaying the dimensions
+    print(str(wid) + "x" + str(hgt))
 
 # Selecting the axis-X making the bottom and top axes False.
 plt.tick_params(axis='x', which='both', bottom=False,
